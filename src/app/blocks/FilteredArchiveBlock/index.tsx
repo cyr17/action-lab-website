d4a0bc0fc274bac2374e2cd04188768f47886ea3
cyr17 <73247453+cyr17@users.noreply.github.com>
2024-09-11 20:56:05 +1000

    filteredblocks

---
+++ src/app/blocks/FilteredArchiveBlock/index.tsx
@@ -0,0 +1,62 @@
+import type { Post } from 'src/payload-types'
+
+import configPromise from '@payload-config'
+import { getPayloadHMR } from '@payloadcms/next/utilities'
+import React from 'react'
+import RichText from 'src/app/components/RichText'
+
+import { CollectionArchive } from '../../components/CollectionArchive'
+import { FilteredArchiveBlockProps } from './types'
+
+export const ArchiveBlock: React.FC<
+  FilteredArchiveBlockProps & {
+    id?: string,
+    collection: 'posts' | 'solutions' | 'caseStudies' | 'impactAreas' | 'people',
+    impactAreas: any[],
+  }
+> = async (props) => {
+  const { id,impactAreas, introContent, limit = 3, populateBy, selectedDocs, relationTo } = props
+
+  let items: any[] = []
+
+  if (populateBy === 'collection') {
+    const payload = await getPayloadHMR({ config: configPromise })
+
+    const flattenedImpactAreas = impactAreas.map((impactArea) => {
+      if (typeof impactArea === 'object') return impactArea.id
+      else return impactArea
+    })
+
+    const fetchedItems = await payload.find({
+      collection: relationTo,
+      depth: 1,
+      limit,
+      ...(flattenedImpactAreas && flattenedImpactAreas.length > 0
+        ? {
+            where: {
+              impactAreas: {
+                in: flattenedImpactAreas,
+              },
+            },
+          }
+        : {}),
+    })
+
+    items = fetchedItems.docs
+  } else {
+    items = selectedDocs.map((item) => {
+      if (typeof item.value === 'object') return item.value
+    })
+  }
+
+  return (
+    <div className="my-16" id={`block-${id}`}>
+      {introContent && (
+        <div className="container mb-16">
+          <RichText className="ml-0 max-w-[48rem]" content={introContent} enableGutter={false} />
+        </div>
+      )}
+      <CollectionArchive items={items} collection={relationTo} />
+    </div>
+  )
+}
---
+++ src/payload/blocks/FilteredArchiveBlock/index.ts
@@ -0,0 +1,99 @@
+import type { Block } from 'payload'
+
+import {
+  FixedToolbarFeature,
+  HeadingFeature,
+  InlineToolbarFeature,
+  lexicalEditor,
+} from '@payloadcms/richtext-lexical'
+
+export const FilteredArchive: Block = {
+  slug: 'filteredArchive',
+  fields: [
+    {
+      name: 'introContent',
+      type: 'richText',
+      editor: lexicalEditor({
+        features: ({ rootFeatures }) => {
+          return [
+            ...rootFeatures,
+            HeadingFeature({ enabledHeadingSizes: ['h1', 'h2', 'h3', 'h4'] }),
+            FixedToolbarFeature(),
+            InlineToolbarFeature(),
+          ]
+        },
+      }),
+      label: 'Intro Content',
+    },
+    {
+      name: 'populateBy',
+      type: 'select',
+      defaultValue: 'collection',
+      options: [
+        {
+          label: 'Collection',
+          value: 'collection',
+        },
+        {
+          label: 'Individual Selection',
+          value: 'selection',
+        },
+      ],
+    },
+    {
+      name: 'relationTo',
+      type: 'select',
+      admin: {
+        condition: (_, siblingData) => siblingData.populateBy === 'collection',
+      },
+      defaultValue: 'posts',
+      label: 'Collections To Show',
+      options: [
+        {
+          label: 'Posts',
+          value: 'posts',
+        },
+        {
+          label: 'Case Studies',
+          value: 'caseStudies',
+        },
+        {
+          label: 'Solutions',
+          value: 'solutions',
+        },
+        {
+          label: 'Impact Areas',
+          value: 'impactAreas',
+        },
+        {
+          label: 'People',
+          value: 'people',
+        },
+      ],
+    },
+    {
+      name: 'limit',
+      type: 'number',
+      admin: {
+        condition: (_, siblingData) => siblingData.populateBy === 'collection',
+        step: 1,
+      },
+      defaultValue: 10,
+      label: 'Limit',
+    },
+    {
+      name: 'selectedDocs',
+      type: 'relationship',
+      admin: {
+        condition: (_, siblingData) => siblingData.populateBy === 'selection',
+      },
+      hasMany: true,
+      label: 'Selection',
+      relationTo: ['posts','caseStudies','solutions','impactAreas','people'],
+    },
+  ],
+  labels: {
+    plural: 'Archives',
+    singular: 'Archive',
+  },
+}
