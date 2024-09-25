2bd7e5155e6d341121575736f2807906926a2d92
cyr17 <73247453+cyr17@users.noreply.github.com>
2024-09-25 21:37:22 +1000

    Chore: Fixed media indentation issue

--- src/app/blocks/MediaBlock/index.tsx
+++ src/app/blocks/MediaBlock/index.tsx
@@ -1,22 +1,19 @@
-import type { StaticImageData } from 'next/image'
-
-import { cn } from '@/utilities/cn'
-import React from 'react'
-import RichText from 'src/app/components/RichText'
-
-import type { Page } from '../../../payload-types'
-
-import { Media } from '../../components/Media'
+import type { StaticImageData } from 'next/image';
+import { cn } from '@/utilities/cn';
+import React from 'react';
+import RichText from 'src/app/components/RichText';
+import type { Page } from '../../../payload-types';
+import { Media } from '../../components/Media';
 
 type Props = Extract<Page['layout'][0], { blockType: 'mediaBlock' }> & {
-  breakout?: boolean
-  captionClassName?: string
-  className?: string
-  enableGutter?: boolean
-  id?: string
-  imgClassName?: string
-  staticImage?: StaticImageData
-}
+  breakout?: boolean;
+  captionClassName?: string;
+  className?: string;
+  enableGutter?: boolean;
+  id?: string;
+  imgClassName?: string;
+  staticImage?: StaticImageData;
+};
 
 export const MediaBlock: React.FC<Props> = (props) => {
   const {
@@ -27,42 +24,37 @@ export const MediaBlock: React.FC<Props> = (props) => {
     media,
     position = 'default',
     staticImage,
-  } = props
+  } = props;
 
-  let caption
-  if (media && typeof media === 'object') caption = media.caption
+  let caption;
+  if (media && typeof media === 'object') caption = media.caption;
+
+  const isFullscreen = position === 'fullscreen';
 
   return (
     <div
       className={cn(
-        '',
+        isFullscreen
+          ? 'relative left-1/2 right-1/2 -mx-[50vw] w-screen max-w-none'
+          : '',
         {
-          container: position === 'default' && enableGutter,
+          container: !isFullscreen && enableGutter,
         },
         className,
       )}
     >
-      {position === 'fullscreen' && (
-        <div className="relative">
-          <Media resource={media} src={staticImage} />
-        </div>
-      )}
-      {position === 'default' && (
-        <Media imgClassName={cn('rounded', imgClassName)} resource={media} src={staticImage} />
-      )}
+      <Media
+        imgClassName={cn('w-full', !isFullscreen && 'rounded', imgClassName)}
+        resource={media}
+        src={staticImage}
+      />
+
+      
       {caption && (
-        <div
-          className={cn(
-            'mt-6',
-            {
-              container: position === 'fullscreen',
-            },
-            captionClassName,
-          )}
-        >
-          <RichText content={caption} enableGutter={false} />
-        </div>
+        <div className="mt-6 mx-auto text-center">
+        <RichText content={caption} enableGutter={false} />
+      </div>
       )}
     </div>
-  )
-}
+  );
+};
