e965b9ef338917a9c12f76c092f6b29be43513b8
cyr17 <73247453+cyr17@users.noreply.github.com>
2024-09-26 10:42:51 +1000

    Feat: Card Layout to seperate top fold

--- src/app/heros/SolutionHero/index.tsx
+++ src/app/heros/SolutionHero/index.tsx
@@ -12,8 +12,8 @@ export const SolutionHero: React.FC<{
 
   return (
     <div className="relative -mt-[10.4rem] flex items-end">
-      <div className="container z-10 relative lg:grid lg:grid-cols-[1fr_48rem_1fr] text-white pb-8">
-        <div className="col-start-1 col-span-1 md:col-start-2 md:col-span-2">
+    <div className="container z-10 relative text-white pb-8 mx-auto lg:max-w-4xl">
+      <div className="bg-black dark:bg-card text-white border-t border-border p-8 rounded-lg shadow-lg">
           <div className="uppercase text-sm mb-6">
             {impactAreas?.map((category, index) => {
               if (typeof category === 'object' && category !== null) {
