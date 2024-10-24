2199eec17a58f2dc9bbc3e9a8999592f2b22bbaf
cyr17 <73247453+cyr17@users.noreply.github.com>
2024-10-24 16:16:49 +1100

    Chore: Ui fix for casestudies page

--- src/app/heros/CaseStudyHero/index.tsx
+++ src/app/heros/CaseStudyHero/index.tsx
@@ -11,61 +11,34 @@ export const CaseStudyHero: React.FC<{
   const { impactAreas, meta: { image: metaImage } = {}, populatedAuthors, publishedAt, title } = caseStudy
 
   return (
-    <div className="relative -mt-[10.4rem] flex items-end">
-    <div className="container z-10 relative text-white pb-8 mx-auto lg:max-w-4xl">
-      <div className="bg-black text-white border-t border-border p-8 rounded-lg shadow-lg">
-          <div className="uppercase text-sm mb-6">
-            {impactAreas?.map((category, index) => {
-              if (typeof category === 'object' && category !== null) {
-                const { title: categoryTitle } = category
-
-                const titleToUse = categoryTitle || 'Untitled category'
-
-                const isLast = index === impactAreas.length - 1
-
-                return (
-                  <React.Fragment key={index}>
-                    {titleToUse}
-                    {!isLast && <React.Fragment>, &nbsp;</React.Fragment>}
-                  </React.Fragment>
-                )
-              }
-              return null
-            })}
-          </div>
-
+    <div className="relative mt-[6rem] flex items-end">
+    
+    <div className="container z-10 relative text-black pb-8 mx-auto lg:max-w-4xl">
           <div className="">
             <h1 className="mb-6 text-3xl md:text-5xl lg:text-6xl">{title}</h1>
           </div>
 
           <div className="flex flex-col md:flex-row gap-4 md:gap-16">
             <div className="flex flex-col gap-4">
-              {populatedAuthors && (
-                <div className="flex flex-col gap-1">
-                  <p className="text-sm">Author</p>
-                  {populatedAuthors.map((author, index) => {
-                    const { name } = author
+              <div className="uppercase text-sm mb-6">
+                {impactAreas?.map((category, index) => {
+                  if (typeof category === 'object' && category !== null) {
+                    const { title: categoryTitle } = category
 
-                    const isLast = index === populatedAuthors.length - 1
-                    const secondToLast = index === populatedAuthors.length - 2
+                    const titleToUse = categoryTitle || 'Untitled category'
+
+                    const isLast = index === impactAreas.length - 1
 
                     return (
                       <React.Fragment key={index}>
-                        {name}
-                        {secondToLast && populatedAuthors.length > 2 && (
-                          <React.Fragment>, </React.Fragment>
-                        )}
-                        {secondToLast && populatedAuthors.length === 2 && (
-                          <React.Fragment> </React.Fragment>
-                        )}
-                        {!isLast && populatedAuthors.length > 1 && (
-                          <React.Fragment>and </React.Fragment>
-                        )}
+                        {titleToUse}
+                        {!isLast && <React.Fragment>, &nbsp;</React.Fragment>}
                       </React.Fragment>
                     )
-                  })}
-                </div>
-              )}
+                  }
+                  return null
+                })}
+              </div>
             </div>
             {publishedAt && (
               <div className="flex flex-col gap-1">
@@ -75,13 +48,7 @@ export const CaseStudyHero: React.FC<{
               </div>
             )}
           </div>
-        </div>
-      </div>
-      <div className="min-h-[80vh] select-none">
-        {metaImage && typeof metaImage !== 'string' && (
-          <Media fill imgClassName="-z-10 object-cover" resource={metaImage} />
-        )}
-        <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent" />
+        
       </div>
     </div>
   )
