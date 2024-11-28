fd3d4d7610839790119ca9ea891616841849d497
cyr17 <73247453+cyr17@users.noreply.github.com>
2024-11-28 16:49:55 +1100

    Chore: Typecasting null reference

--- src/app/components/FooterMedia/index.tsx
+++ src/app/components/FooterMedia/index.tsx
@@ -6,14 +6,16 @@ import type { FooterMedia, Media } from '../../../payload-types'
 
 export async function FooterMedia() {
     const footerMedia= await getCachedGlobal('footer-media', 1)() as FooterMedia;
-    const media = footerMedia.media as Media;
+    const media = footerMedia.media as Media|undefined;
   return (
     <div>
         <div className="h-fit border-0 select-none">
         {footerMedia && typeof footerMedia !== 'string' && (
             
             <div className='relative -bottom-2'>
+                {media && media.url && (
                 <img className="-z-10 object-cover" src={media.url} />
+                )}
                 <div className="absolute pointer-events-none left-0 bottom-0 w-full h-1/2 bg-gradient-to-t from-black to-transparent"/>
             </div>
         )}
